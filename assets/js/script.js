f44a4891b79411767ee234da2454a5cad48bf227
StevenD <dorvilsteven@gmail.com>
2021-09-02 14:30:54 -0400

    questions and answers displayed, next style them and display 1 at a time

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,82 +7,81 @@ var questions = [
     {   
         type: 'HTML',
         question: 'Which of the following is not considered an HTML semantic tag?',
-        choice1: '<article>',
-        choice2: '<part>',
-        choice3: '<section>',
-        choice4: '<nav>',
+        choices: ['<article>','<part>','<section>','<nav>'],
+        answer: 1
     },{
         type: 'HTML',
-        question: 'Which of the following is an HTML tag attribute?',   
-        choice1: 'display',
-        choice2: 'style',
-        choice3: 'meta',
-        choice4: 'color'
+        question: 'Which of the following is an HTML tag attribute?',
+        choices: ['display','style','meta','color'],
+        answer: 1
     },{
         type: 'CSS',
         question: 'A container is given a display of flex, which is not a valid value for the justify-content property?',
-        choice1: 'center',
-        choice2: 'space-between',
-        choice3: 'flex-end',
-        choice4: 'flex-around'
+        choices: ['center','space-between','flex-end','flex-around'],
+        answer: 3
     },{
         type: 'JavaScript',
         question: 'What is the syntax for a comment is JavaScript?',
-        choice1: '//, /**/',
-        choice2: '<!-- -->',
-        choice3: 'document.comment()',
-        choice4: 'var comment = '  
+        choices: ['//, /**/','<!-- -->','document.comment()','var comment ='],
+        answer: 0  
     },{
         type: 'CSS',
         question: 'How can we select this tag "<h2 id="header2" class="article-title">Title of Artitle</h2>"?',
-        choice1: '#TitleofArticle',
-        choice2: '.h2',
-        choice3: '.article-title',
-        choice4: 'header2'
+        choices: ['#TitleofArticle','.h2','.article-title','header2'],
+        answer: 2
     },{
         type: 'JavaScript',
         question: 'What is the value of "z" in the following code? \n var x = "3";\nvar y = "3";\n var z = x + y;',
-        choice1: 'undefined',
-        choice2: '9',
-        choice3: '6',
-        choice4: '33'
+        choices: ['undefined','9','6','33'],
+        answer: 3
     },{
         type: 'HTML',
         question: 'Which element is an inline HTML element?',
-        choice1: '<div>',
-        choice2: '<form>',
-        choice3: '<img>',
-        choice4: '<nav>'
+        choices: ['<div>','<form>','<img>','<nav>'],
+        asnwer: 2
     },{
         type: 'JavaScript',
         question: 'Which is not a object in JavaScript?',
-        choice1: 'window',
-        choice2: 'for',
-        choice3: 'MATH',
-        choice4: 'document'
+        choices: ['window','for','MATH','document'],
+        asnwer: 1
     },{
         type: 'HTML',
         question: 'Where should you link your external stylesheet in your HTML file?',
-        choice1: 'at the bottom of the <body> tag',
-        choice2: 'at the very top of the file',
-        choice3: 'at the top of the <body> tag',
-        choice4: 'at the bottom of the <head> tag'
+        choices: ['at the bottom of the <body> tag','at the very top of the file','at the top of the <body> tag','at the bottom of the <head> tag'],
+        answer: 3
     },{
         type: 'CSS',
         question: 'How do you reference a variable in CSS?',
-        choice1: 'var(--variable-name)',
-        choice2: 'variable-name',
-        choice3: '.variable-name',
-        choice4: '#variable-name'
+        choices: ['var(--variable-name)','variable-name','.variable-name','#variable-name'],
+        answer: 0
     }];
 
+var displayQuestions = function(question) {
+    // create question div
+    var questionEl = document.createElement('div');
+    var h1El = document.createElement('h1');
+    h1El.textContent = question.question;
+    questionEl.appendChild(h1El);
+    quiz.appendChild(questionEl);
 
+    // create answer list div
+    var answersEl = document.createElement('div');
+    var answerList = document.createElement('ul');
+    for (var i=0;i<4;i++) {
+        var listEl = document.createElement('li');
+        var choice = question.choices[i];
+        listEl.textContent = choice;
+        answerList.appendChild(listEl);
+    }
+    answersEl.appendChild(answerList);
+    quiz.appendChild(answersEl);
+}
 var startGame = function() {
     homepage.classList.add('no-display');
     quiz.classList.remove('no-display');
     // game logic
     for (var i=0;i<questions.length;i++) {
-        console.log(questions[i]);
+        displayQuestions(questions[i]);
     }
 };
 
